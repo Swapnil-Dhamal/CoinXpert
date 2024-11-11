8c89eb71e48972ade38d4ef49c94e0d258da98ce
Swapnil Dhamal <swapnildhamal58@gmail.com>
2024-11-11 12:13:57 +0530

    Verification code service

---
+++ src/main/java/com/swapnil/TradingApp/repo/VerificationCodeRepo.java
@@ -0,0 +1,11 @@
+package com.swapnil.TradingApp.repo;
+
+import com.swapnil.TradingApp.model.VerificationCode;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface VerificationCodeRepo extends JpaRepository<VerificationCode, Long> {
+
+    public VerificationCode findByUserId(Long userId);
+}
