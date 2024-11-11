5fbcc3def75e22713a08c8810c9aa94b2abbc1b6
Swapnil Dhamal <swapnildhamal58@gmail.com>
2024-11-11 12:12:19 +0530

    Code verification for otp

---
+++ src/main/java/com/swapnil/TradingApp/model/VerificationCode.java
@@ -0,0 +1,26 @@
+package com.swapnil.TradingApp.model;
+
+
+import com.swapnil.TradingApp.domain.VerificationType;
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Data
+public class VerificationCode {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String otp;
+
+    @OneToOne
+    private Users user;
+
+    private String email;
+
+    private String mobile;
+
+    private VerificationType verificationType;
+}
