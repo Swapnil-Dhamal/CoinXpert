1bed209a0ff4847a8895cb7e1e9409b990e3a869
Swapnil Dhamal <swapnildhamal58@gmail.com>
2024-11-15 18:34:03 +0530

    Wallet entity

---
+++ src/main/java/com/swapnil/TradingApp/model/Wallet.java
@@ -0,0 +1,20 @@
+package com.swapnil.TradingApp.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.math.BigDecimal;
+
+@Entity
+@Data
+public class Wallet {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @OneToOne
+    private Users user;
+
+    private BigDecimal balance;
+}
