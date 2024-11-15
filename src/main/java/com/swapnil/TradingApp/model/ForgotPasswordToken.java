f26d7d35556a159e29bfaf5228cdf34777d2b0b8
Swapnil Dhamal <swapnildhamal58@gmail.com>
2024-11-15 08:44:52 +0530

    Forgot password entity

---
+++ src/main/java/com/swapnil/TradingApp/model/ForgotPasswordToken.java
@@ -0,0 +1,24 @@
+package com.swapnil.TradingApp.model;
+
+
+import com.swapnil.TradingApp.domain.VerificationType;
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Data
+public class ForgotPasswordToken {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private String id;
+
+    @OneToOne
+    private Users user;
+
+    private String otp;
+
+    private VerificationType verificationType;
+
+    private String sendTo;
+}
