d4d762165614a83e88758a63f8d27787c4efc7ec
Swapnil Dhamal <swapnildhamal58@gmail.com>
2024-11-20 10:55:15 +0530

    Implementation for payment deails service

---
+++ src/main/java/com/swapnil/TradingApp/service/impl/PaymentDetailsServiceImpl.java
@@ -0,0 +1,40 @@
+package com.swapnil.TradingApp.service.impl;
+
+import com.swapnil.TradingApp.model.PaymentDetails;
+import com.swapnil.TradingApp.model.Users;
+import com.swapnil.TradingApp.repo.PaymentDetailsRepo;
+import com.swapnil.TradingApp.service.PaymentDetailsService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+
+@Service
+@RequiredArgsConstructor
+public class PaymentDetailsServiceImpl implements PaymentDetailsService {
+
+    private final PaymentDetailsRepo paymentDetailsRepo;
+
+    @Override
+    public PaymentDetails addUserDetails(String accountNumber,
+                                         String accountHolderName,
+                                         String ifsc,
+                                         String bankName,
+                                         Users user) {
+
+        PaymentDetails paymentDetails=new PaymentDetails();
+
+        paymentDetails.setAccountNumber(accountNumber);
+        paymentDetails.setAccountHolderName(accountHolderName);
+        paymentDetails.setIfsc(ifsc);
+        paymentDetails.setBankName(bankName);
+        paymentDetails.setUsers(user);
+        return paymentDetailsRepo.save(paymentDetails);
+    }
+
+    @Override
+    public PaymentDetails getUserPaymentDetails(Users user) {
+
+        return paymentDetailsRepo.findBuUserId(user.getId());
+
+    }
+}
