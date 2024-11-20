1ec8baa40f23294f600ff43192119fd0721777c5
Swapnil Dhamal <swapnildhamal58@gmail.com>
2024-11-20 10:56:05 +0530

    Payment details repository

---
+++ src/main/java/com/swapnil/TradingApp/repo/PaymentDetailsRepo.java
@@ -0,0 +1,11 @@
+package com.swapnil.TradingApp.repo;
+
+import com.swapnil.TradingApp.model.PaymentDetails;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PaymentDetailsRepo extends JpaRepository<PaymentDetails, Long> {
+
+    PaymentDetails findBuUserId(Long userId);
+}
