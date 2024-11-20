3042c2b4e441ae51d5bee0145df33a37674d96b2
Swapnil Dhamal <swapnildhamal58@gmail.com>
2024-11-20 19:01:07 +0530

    PaymentDetails model

---
+++ src/main/java/com/swapnil/TradingApp/model/PaymentDetails.java
@@ -0,0 +1,27 @@
+package com.swapnil.TradingApp.model;
+
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Data
+@Entity
+public class PaymentDetails {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String accountNumber;
+
+    private String accountHolderName;
+
+    private String bankName;
+
+    private String ifsc;
+
+    @OneToOne
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
+    private Users users;
+}
