debf37debfe7ddb1a32a4fafeb084a7c82c149c1
Swapnil Dhamal <swapnildhamal58@gmail.com>
2024-11-21 19:48:40 +0530

    Payment repository

---
+++ src/main/java/com/swapnil/TradingApp/repo/PaymentRepo.java
@@ -0,0 +1,10 @@
+package com.swapnil.TradingApp.repo;
+
+
+import com.swapnil.TradingApp.model.PaymentOrder;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PaymentRepo extends JpaRepository<PaymentOrder, Long> {
+}
