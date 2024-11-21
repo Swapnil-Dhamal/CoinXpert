d951665c52debc9d65b77b1cccae5039769a2c2e
Swapnil Dhamal <swapnildhamal58@gmail.com>
2024-11-21 19:50:18 +0530

    Payment service

---
+++ src/main/java/com/swapnil/TradingApp/service/PaymentService.java
@@ -0,0 +1,21 @@
+package com.swapnil.TradingApp.service;
+
+import com.razorpay.RazorpayException;
+import com.stripe.exception.StripeException;
+import com.swapnil.TradingApp.domain.PaymentMethod;
+import com.swapnil.TradingApp.model.PaymentOrder;
+import com.swapnil.TradingApp.model.Users;
+import com.swapnil.TradingApp.response.PaymentResponse;
+
+public interface PaymentService {
+
+    PaymentOrder createOrder(Users user , Long amount, PaymentMethod paymentMethod);
+
+    PaymentOrder getPaymentOrderById(Long id) throws Exception;
+
+    Boolean proceedPaymentOrder(PaymentOrder paymentOrder, String paymentId) throws RazorpayException;
+
+    PaymentResponse createRazorpayPaymentLink(Users user, Long amount) throws RazorpayException;
+
+    PaymentResponse createStripePaymentLink(Users user, Long amount, Long orderId) throws StripeException;
+}
