40f57f407616f145aebe49f94bbb37ae42598f81
Swapnil Dhamal <swapnildhamal58@gmail.com>
2024-11-21 21:54:18 +0530

    Payment controller

---
+++ src/main/java/com/swapnil/TradingApp/controller/PaymentController.java
@@ -0,0 +1,79 @@
+package com.swapnil.TradingApp.controller;
+
+
+import com.razorpay.Payment;
+import com.razorpay.RazorpayException;
+import com.stripe.exception.StripeException;
+import com.swapnil.TradingApp.domain.PaymentMethod;
+import com.swapnil.TradingApp.model.Order;
+import com.swapnil.TradingApp.model.PaymentOrder;
+import com.swapnil.TradingApp.model.Users;
+import com.swapnil.TradingApp.model.Wallet;
+import com.swapnil.TradingApp.response.PaymentResponse;
+import com.swapnil.TradingApp.service.OrderService;
+import com.swapnil.TradingApp.service.PaymentService;
+import com.swapnil.TradingApp.service.UserService;
+import com.swapnil.TradingApp.service.WalletService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api")
+@RequiredArgsConstructor
+public class PaymentController {
+
+    private final UserService userService;
+    private final PaymentService paymentService;
+    private final WalletService walletService;
+    private final OrderService orderService;
+
+
+    @PostMapping("/payment/{paymentMethod}/amount/{amount}")
+    public ResponseEntity<PaymentResponse> paymentHandler(
+
+            @RequestHeader("Authorization") String jwt,
+            @PathVariable PaymentMethod paymentMethod,
+            @PathVariable Long amount
+            ) throws RazorpayException, StripeException {
+        Users user=userService.findUserProfileByJwt(jwt);
+
+        PaymentResponse paymentResponse;
+
+        PaymentOrder paymentOrder=paymentService.createOrder(user, amount, paymentMethod);
+
+        if(paymentMethod.equals(PaymentMethod.RAZORPAY)){
+            paymentResponse = paymentService.createRazorpayPaymentLink(user, amount);
+        }
+        else{
+            paymentResponse = paymentService.createStripePaymentLink(user, amount, Long.valueOf(paymentOrder.getId()));
+        }
+
+        return new ResponseEntity<>(paymentResponse, HttpStatus.CREATED);
+    }
+
+    @PutMapping("/wallet/deposit")
+    public ResponseEntity<Wallet> addBalanceToWallet(
+            @RequestHeader("Authorization") String jwt,
+            @RequestParam(name = "order_id") Long orderId,
+            @RequestParam(name = "payment_id") String paymentId
+    ) throws Exception {
+
+        Users user=userService.findUserProfileByJwt(jwt);
+        Wallet wallet=walletService.getUserWallet(user);
+        PaymentOrder order=paymentService.getPaymentOrderById(orderId);
+
+        Boolean status = paymentService.proceedPaymentOrder(order, paymentId);
+
+        if(status){
+            wallet= walletService.addBalance(wallet, order.getAmount());
+        }
+
+        return  new ResponseEntity<>(wallet, HttpStatus.ACCEPTED);
+
+
+
+    }
+
+}
