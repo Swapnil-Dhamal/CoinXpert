6ac505cce70208074dc455fe9f05f0cb4cb1eb1d
Swapnil Dhamal <swapnildhamal58@gmail.com>
2025-02-05 16:59:49 +0530

    Created Navbar and Sidebar

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,16 +1,14 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
+import { Sidebar } from 'lucide-react'
 import './App.css'
-import { Button } from './components/ui/button'
 import { Navbar } from './page/Navbar/Navbar'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
     <Navbar/>
+    <Sidebar/>
     </>
   )
 }
