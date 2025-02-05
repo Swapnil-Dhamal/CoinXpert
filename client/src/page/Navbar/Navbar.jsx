4f71745844bce0c1499fef0f78d1fa4faa583181
Swapnil Dhamal <swapnildhamal58@gmail.com>
2025-02-05 16:58:11 +0530

    Navbar

--- client/src/page/Navbar/Navbar.jsx
+++ client/src/page/Navbar/Navbar.jsx
@@ -1,21 +1,41 @@
 import React from "react";
+import { Sheet, SheetTrigger, SheetContent, SheetHeader, SheetTitle, SheetDescription } from "@/components/ui/sheet";
+import { FaBars } from 'react-icons/fa';  
+import { Avatar, AvatarImage } from "@radix-ui/react-avatar";
+import CryptoLogo from "../../page/images/logo.png";
+import { Spa } from "@mui/icons-material";
 
 export const Navbar = () => {
   return (
     <div
-      className="px-2 py-3 border-b z-50 bg-background bg-opacity-0 sticky top-0 left-0
-    right-0 flex justify-between items-center"
+      className="w-screen fixed top-0 left-0 border-b z-50 bg-background bg-opacity-0 
+      flex items-center justify-start p-3"
     >
-      <div>
+      {/* Hamburger icon positioned at the extreme left */}
+      <div className="flex items-center gap-3">
         <Sheet>
-          <SheetTrigger>Open</SheetTrigger>
-          <SheetContent>
+          <SheetTrigger asChild>
+            <div className="rounded-full h-11 w-11 flex items-center justify-center cursor-pointer">
+              <FaBars className="h-7 w-7"/>
+            </div>
+          </SheetTrigger>
+
+          <SheetContent className="w-72 border-r-0 flex flex-col justify-center"
+          side='left'>
             <SheetHeader>
-              <SheetTitle>Are you absolutely sure?</SheetTitle>
-              <SheetDescription>
-                This action cannot be undone. This will permanently delete your
-                account and remove your data from our servers.
-              </SheetDescription>
+              <SheetTitle>
+                <div className="text-3xl flex justify-center items-center gap-1">
+                  <Avatar>
+                    <AvatarImage src={CryptoLogo} alt="Logo" className="w-10 h-10 rounded-full"></AvatarImage>
+                  </Avatar>
+                  <div>
+                    <span className="font-bold text-orange-500">Coin</span>
+                    <span>Xpert</span>
+
+                  </div>
+                </div>
+              </SheetTitle>
+              
             </SheetHeader>
           </SheetContent>
         </Sheet>
@@ -23,3 +43,5 @@ export const Navbar = () => {
     </div>
   );
 };
+
+export default Navbar;
