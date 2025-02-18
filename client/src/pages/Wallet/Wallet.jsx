bd9f45f31a16c8333fdb9d4926910879086ba5fe
Swapnil Dhamal <swapnildhamal58@gmail.com>
2025-02-18 18:26:57 +0530

    Added wallet history

--- client/src/pages/Wallet/Wallet.jsx
+++ client/src/pages/Wallet/Wallet.jsx
@@ -1,11 +1,12 @@
 import { Card, CardContent, CardHeader } from "@/components/ui/card";
 import { Dialog, DialogContent, DialogHeader, DialogTitle, DialogTrigger } from "@/components/ui/dialog";
-import { ReloadIcon, UploadIcon } from "@radix-ui/react-icons";
+import { ReloadIcon, UpdateIcon } from "@radix-ui/react-icons";
 import { CopyIcon, DollarSign, Download, ShuffleIcon, Upload, WalletIcon } from "lucide-react";
 import React from "react";
 import TopUpForm from "./TopUpForm";
 import WithdrawalForm from "./WithdrawalForm";
 import TransferForm from "./TransferForm";
+import { Avatar, AvatarFallback } from "@/components/ui/avatar";
 
 const Wallet = () => {
   return (
@@ -33,19 +34,16 @@ const Wallet = () => {
           </CardHeader>
 
           <CardContent>
-            <div className="flex items-center">
-              <DollarSign/>
-              <span className="text-2xl font-semibold">
-                45769
-              </span>
+            <div className="flex items-center gap-2">
+              <DollarSign />
+              <span className="text-2xl font-semibold">45769</span>
             </div>
 
             <div className="flex gap-7 mt-5">
+              {/* Add Money */}
               <Dialog>
                 <DialogTrigger>
-                  <div className="h-24 w-24 hover:text-gray-400 cursor-pointer
-                  flex flex-col items-center justify-center rounded-md shadow-slate-800">
-
+                  <div className="h-24 w-24 hover:text-gray-400 cursor-pointer flex flex-col items-center justify-center rounded-md shadow-md">
                     <Upload />
                     <span className="text-sm mt-2">Add Money</span>
                   </div>
@@ -53,39 +51,33 @@ const Wallet = () => {
 
                 <DialogContent>
                   <DialogHeader>
-                    <DialogTitle>
-                      Top Up Your Wallet
-                    </DialogTitle>
+                    <DialogTitle>Top Up Your Wallet</DialogTitle>
                   </DialogHeader>
-                  <TopUpForm/>
+                  <TopUpForm />
                 </DialogContent>
               </Dialog>
 
+              {/* Withdrawal */}
               <Dialog>
                 <DialogTrigger>
-                  <div className="h-24 w-24 hover:text-gray-400 cursor-pointer
-                  flex flex-col items-center justify-center rounded-md shadow-slate-800">
-
+                  <div className="h-24 w-24 hover:text-gray-400 cursor-pointer flex flex-col items-center justify-center rounded-md shadow-md">
                     <Download />
-                    <span className="text-sm mt-2">Withdrawal</span>
+                    <span className="text-sm mt-2">Withdraw</span>
                   </div>
                 </DialogTrigger>
 
                 <DialogContent>
                   <DialogHeader>
-                    <DialogTitle>
-                      Request Withdrawal
-                    </DialogTitle>
+                    <DialogTitle>Request Withdrawal</DialogTitle>
                   </DialogHeader>
-                  <WithdrawalForm/>
+                  <WithdrawalForm />
                 </DialogContent>
               </Dialog>
 
+              {/* Transfer */}
               <Dialog>
                 <DialogTrigger>
-                  <div className="h-24 w-24 hover:text-gray-400 cursor-pointer
-                  flex flex-col items-center justify-center rounded-md shadow-slate-800">
-
+                  <div className="h-24 w-24 hover:text-gray-400 cursor-pointer flex flex-col items-center justify-center rounded-md shadow-md">
                     <ShuffleIcon />
                     <span className="text-sm mt-2">Transfer</span>
                   </div>
@@ -93,16 +85,47 @@ const Wallet = () => {
 
                 <DialogContent>
                   <DialogHeader>
-                    <DialogTitle>
-                      Transfer to other Wallet
-                    </DialogTitle>
+                    <DialogTitle>Transfer to Another Wallet</DialogTitle>
                   </DialogHeader>
-                  <TransferForm/>
+                  <TransferForm />
                 </DialogContent>
               </Dialog>
             </div>
           </CardContent>
         </Card>
+
+        {/* Transaction History */}
+        <div className="py-5 pt-10">
+          <div className="flex gap-2 items-center pb-5">
+            <h1 className="text-2xl font-semibold">History</h1>
+            <UpdateIcon className="h-6 w-6 cursor-pointer hover:text-gray-400" />
+          </div>
+
+          <div className="space-y-5">
+            {[1, 1, 1, 1, 1, 1, 1, 1, 1, 1].map((item, index) => (
+              <div key={index}>
+                <Card className="px-5 flex justify-between items-center">
+                  <div className="flex items-center gap-5">
+                    <Avatar>
+                      <AvatarFallback>
+                        <ShuffleIcon />
+                      </AvatarFallback>
+                    </Avatar>
+
+                    <div className="p-2">
+                      <h1>Buy Asset</h1>
+                      <p className="text-sm text-gray-500">2024-05-23</p>
+                    </div>
+                  </div>
+
+                  <div>
+                    <p className="text-green-500">600$</p>
+                  </div>
+                </Card>
+              </div>
+            ))}
+          </div>
+        </div>
       </div>
     </div>
   );
