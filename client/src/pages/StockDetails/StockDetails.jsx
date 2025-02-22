6fca43d0d5a1e816abde27fd6dae6789387cc058
Swapnil Dhamal <swapnildhamal58@gmail.com>
2025-02-22 21:41:53 +0530

    Stock details

--- client/src/pages/StockDetails/StockDetails.jsx
+++ client/src/pages/StockDetails/StockDetails.jsx
@@ -1,9 +1,75 @@
-import React from 'react'
+import { Avatar, AvatarImage } from "@/components/ui/avatar";
+import { Button } from "@/components/ui/button";
+import { BookmarkFilledIcon } from "@radix-ui/react-icons";
+import { BookmarkIcon, DotIcon } from "lucide-react";
+import React from "react";
+import {
+  Dialog,
+  DialogTrigger,
+  DialogContent,
+  DialogHeader,
+  DialogTitle,
+  DialogDescription,
+} from "@/components/ui/dialog";
+import TradingForm from "./TradingForm";
+import StockChart from "../Home/StockChart";
 
 const StockDetails = () => {
   return (
-    <div>StockDetails</div>
-  )
-}
+    <div className="p-5 mt-5">
+      <div className="flex justify-between">
+        <div className="flex gap-5 items-center">
+          <div>
+            <Avatar>
+              <AvatarImage
+                src="https://cryptologos.cc/logos/bitcoin-btc-logo.png"
+                alt="Bitcoin"
+              />
+            </Avatar>
+          </div>
 
-export default StockDetails
+          <div>
+            <div className="flex items-center gap-2">
+              <p>BTC</p>
+              <DotIcon className="text-gray-400" />
+              <p className="text-gray-400">Bitcoin</p>
+            </div>
+            <div className="flex items-end gap-2">
+              <p className="text-xl font-bold">$50698</p>
+              <p className="text-red-600">
+                <span>-78436.935</span>
+                <span>(-0.5378%)</span>
+              </p>
+            </div>
+          </div>
+        </div>
+
+        <div className="flex items-center gap-4">
+          <Button>
+            {true ? (
+              <BookmarkFilledIcon className="h-6 w-6" />
+            ) : (
+              <BookmarkIcon className="h-6 w-6" />
+            )}
+          </Button>
+          <Dialog>
+            <DialogTrigger>
+              <Button size="lg">Trade</Button>
+            </DialogTrigger>
+            <DialogContent>
+              <DialogHeader>
+                <DialogTitle>Are you absolutely sure?</DialogTitle>
+                
+              </DialogHeader>
+              <TradingForm/>
+            </DialogContent>
+          </Dialog>
+        </div>
+      </div>
+
+      <StockChart/>
+    </div>
+  );
+};
+
+export default StockDetails;
