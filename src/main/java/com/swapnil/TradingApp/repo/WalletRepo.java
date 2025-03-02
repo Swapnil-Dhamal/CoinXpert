ed454e0f48bc1cd59658ad1ae351d0f53ccf79dd
Swapnil Dhamal <swapnildhamal58@gmail.com>
2025-03-02 19:02:11 +0530

    get user wallet repo

--- src/main/java/com/swapnil/TradingApp/repo/WalletRepo.java
+++ src/main/java/com/swapnil/TradingApp/repo/WalletRepo.java
@@ -1,11 +1,16 @@
 package com.swapnil.TradingApp.repo;
 
 import com.swapnil.TradingApp.model.Wallet;
+import com.swapnil.TradingApp.model.WalletTransaction;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface WalletRepo extends JpaRepository<Wallet, Long> {
 
     Wallet findByUserId(Long userId);
+
+    List<WalletTransaction> findByWallet(Wallet wallet);
 }
